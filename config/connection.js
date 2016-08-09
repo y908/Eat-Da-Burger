c1d84a758c3a548dab5a39ace748c1ffa6765e64
Yana Yakubchik <Yana Yakubchik>
2016-08-09 00:37:01 -0400

    update heroku

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,10 @@ Here is where you make the connection to the database and export and used by the
 var mysql = require('mysql');
 var connection = mysql.createConnection({
   port: 3306,
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'burgers_db'
+  host: 'us-cdbr-iron-east-04.cleardb.net',
+  user: 'b97a99e6714e10',
+  password: 'ff44b7b8',
+  database: 'heroku_56d96a65f3c593c'
 });
 
 connection.connect(function (err) {
